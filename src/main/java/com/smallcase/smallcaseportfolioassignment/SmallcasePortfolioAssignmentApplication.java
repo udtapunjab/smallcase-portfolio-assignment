f19b0e24b13b283e347573b7fba2029975b03c87
Gourav Patidar <gouravpy15@gmail.com>
2022-05-08 19:01:21 +0530

    Initial commit

---
+++ src/main/java/com/smallcase/smallcaseportfolioassignment/SmallcasePortfolioAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.smallcase.smallcaseportfolioassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallcasePortfolioAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallcasePortfolioAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smallcase/smallcaseportfolioassignment/SmallcasePortfolioAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smallcase.smallcaseportfolioassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallcasePortfolioAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
